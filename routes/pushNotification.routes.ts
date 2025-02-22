1c71d7c476d17312d8d1828b6340ae3e7df5a879
ifeanyi4000 <154647295+ifeanyi4000@users.noreply.github.com>
2025-02-22 07:28:54 +0100

    Update pushNotification.routes.ts

--- routes/pushNotification.routes.ts
+++ routes/pushNotification.routes.ts
@@ -1,9 +1,1 @@
-import express from 'express';
-import { saveToken, sendNotification } from '../controlers/sendNotification.controller';
 
-const pushRouter = express.Router();
-
-pushRouter.post('/send-notification', sendNotification);
-pushRouter.post('/save-token', saveToken);
-
-export default pushRouter;
